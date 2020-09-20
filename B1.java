83a428cc6988dd6d9fb3d69547a24cfb3f85beb2
변승현 <67039023+gusanans218@users.noreply.github.com>
2020-09-21 01:17:15 +0900

    20200920
    20200920

---
+++ B1.java
@@ -0,0 +1,101 @@
+package practice0920;
+	
+	import java.util.Random;
+	import java.util.Scanner;
+
+public class B1 {
+	public static void main(String[] args) {
+		Random rand = new Random();
+		Scanner stdIn = new Scanner(System.in);
+		
+//		int n1 = 1+rand.nextInt(9);
+//		int n2 = -1-rand.nextInt(9);
+//		int n3 = 10+rand.nextInt(90);
+//		System.out.println("3개의 난수를 생성했습니다");
+//		System.out.println("한 자리 양의 정수: "+n1);
+//		System.out.println("한 자리 음의 정수: "+n2);
+//		System.out.println("두 자리 양의 정수: "+n3);
+		
+//		System.out.print("정숫값");
+//		int x = stdIn.nextInt();
+//		System.out.println("입력 값의 +-범위의 난수를 생성했습니다");
+//		System.out.println("값은 "+(x-5 +rand.nextInt(11))+"입니다");
+		
+//		System.out.println("성"); String lastName = stdIn.next();
+//		System.out.println("이름"); String firstName = stdIn.next();
+//		System.out.println("안녕하세요  " +   lastName+firstName+"  씨");
+		
+//		System.out.print("정숫값: ");
+//		int n = stdIn.nextInt();
+//		if(n<0)
+//			System.out.println("이 값은 음의 값입니다");
+		
+//		System.out.print("정숫값");
+//		int n = stdIn.nextInt();
+//		int abs;
+//		if(n>=0)
+//			abs = n;
+//		else
+//			abs = -n;
+//		System.out.println("절댓값은 " +abs+"입니다");
+		
+//		System.out.print("변수 A: "); int a = stdIn.nextInt();
+//		System.out.print("변수 B: "); int b = stdIn.nextInt();
+//		if(a%b==0)
+//			System.out.println("B는 A의 약수입니다");
+//		else
+//			System.out.println("B는 A의 약수가 아닙니다");
+		
+//		System.out.print("정숫값");
+//		int n = stdIn.nextInt();
+//		if(n>0)
+//			System.out.println("값이 양수입니다");
+//		else if(0>n)
+//			System.out.println("값이 음수입니다");
+//		else
+//			System.out.println("값이 0입니다");
+		
+//		System.out.print("정숫값");
+//		int n = stdIn.nextInt();
+//		if(n>0)
+//			if(n%10==0)
+//				System.out.println("이 값은 10의 배수입니다");
+//			else
+//				System.out.println("이 값은 10의 배수가 아닙니다");
+//		else
+//			System.out.println("음수를 입력했습니다");
+		
+		
+//		System.out.print("점수: ");
+//		int point = stdIn.nextInt();
+//		if(point>=0&&point<=49)
+//			System.out.println("가");
+//		else if(point>=50&&point<=59)
+//			System.out.println("양");
+//		else if(point>=60&&point<=69)
+//			System.out.println("미");
+//		else if(point>=70&&point<=79)
+//			System.out.println("우");
+//		else if(point>=80&&point<=100)
+//			System.out.println("수");
+//		else
+//			System.out.println("잘못된 점수입니다");
+		
+//		System.out.print("실수 a: "); double a = stdIn.nextDouble();
+//		System.out.print("실수 b: "); double b = stdIn.nextDouble();
+//		double max;
+//		if(a>b)
+//			max =a;
+//		else
+//			max = b;
+//		System.out.println("큰 쪽의 값은 "+ max+"입니다");
+		
+//		System.out.print("정수 a : "); int a = stdIn.nextInt();
+//		System.out.print("정수 b : "); int b = stdIn.nextInt();
+//		int diff= a>=b?a-b:b-a;
+//		System.out.println("두 값의 차는"+diff+"입니다");
+//		
+		
+	}
+	
+}
