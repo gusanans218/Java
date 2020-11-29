ab9a4591ea3620c439052011731d248274e76eb5
변승현 <67039023+gusanans218@users.noreply.github.com>
2020-11-29 14:02:43 +0900

    2020 자바 방과후 4

--- CarTest.java
+++ CarTest.java
@@ -1,32 +1,34 @@
-package exam02;
+package ch6;
 
 class Car{
-    String color;
-    static int numofcar;
-    static int numofyellowcar;
+	String color;
+	String gearType;
+	int door;
+	
 
-    Car(String color){
-        this.color = color;
-        numofcar ++;
-        if(color == "yellow" || color == "YELLOW") {
-            numofyellowcar ++;
-        }
-    }
-    static int getnumofcar() {
-        return numofcar;
-    }
-    static int getnumofyellowcar() {
-        return numofyellowcar;
-    }
+Car(String c, String g, int d){
+	color = c;
+	gearType = g;
+	door = d;  
 }
 
-public class CarTest {
 
-    public static void main(String[] args) {
-        Car c1 = new Car("yellow");
-        Car c2 = new Car("red");
-        Car c3 = new Car("YELLOW");
-        System.out.printf("자동차수 : %d ,노란색 자동차 수:%d",Car.getnumofcar(), Car.getnumofyellowcar());
-    }
-
+public Car() {
+	// TODO Auto-generated constructor stub
 }
+}
+class CarTest{
+
+	public static void main(String[] args) {
+		Car c1 = new Car();
+		c1.color = "white";
+		c1.gearType = "auto";
+		c1.door = 4;
+		
+		Car c2 = new Car("white", "auto", 4);
+		System.out.println("c1의 color ="+c1.color+", gearType="+c1.gearType+", door ="+c1.door);
+		System.out.println("c2의 color ="+c2.color+", gearType="+c2.gearType+", door ="+c2.door);
+		
+	}
+}
+
