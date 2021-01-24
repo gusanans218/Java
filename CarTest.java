d37d5f57a05cfc47bffd7c299c16eef12ccf2eb9
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-01-24 22:19:09 +0900

    20210102
    20210102

--- CarTest.java
+++ CarTest.java
@@ -1,34 +1,26 @@
-package ch6;
+package coding0102;
 
 class Car{
 	String color;
 	String gearType;
 	int door;
-	
-
-Car(String c, String g, int d){
-	color = c;
-	gearType = g;
-	door = d;  
+	Car(){}
+	Car(String c, String g, int d){
+		color = c;
+		gearType = g;
+		door = d;
+	}
 }
-
-
-public Car() {
-	// TODO Auto-generated constructor stub
-}
-}
-class CarTest{
-
-	public static void main(String[] args) {
-		Car c1 = new Car();
-		c1.color = "white";
+ class CarTest {
+	 public static void main(String[] args) {
+		Car c1 =new Car();
+		c1.color = "White";
 		c1.gearType = "auto";
 		c1.door = 4;
 		
-		Car c2 = new Car("white", "auto", 4);
-		System.out.println("c1의 color ="+c1.color+", gearType="+c1.gearType+", door ="+c1.door);
-		System.out.println("c2의 color ="+c2.color+", gearType="+c2.gearType+", door ="+c2.door);
-		
+		Car c2 = new Car("white", "auto",4);
+		System.out.println("c1의 color = "+c1.color+", gearType="+c1.gearType+", door="+c1.door);
+		System.out.println("c2의 color = "+c2.color+", gearType="+c2.gearType+", door="+c2.door);
 	}
-}
 
+}
---
+++ OverloadingTest.java
@@ -0,0 +1,29 @@
+package coding0102;
+
+public class OverloadingTest {
+	public static void main(String[] args) {
+		MyMath3 mm = new MyMath3();
+		System.out.println("mm.add(3,3) 결과 :"+mm.add(3,3));
+		System.out.println("mm.add(3L,3) 결과 :"+mm.add(3L,3));
+		System.out.println("mm.add(3,3L) 결과 :"+mm.add(3,3L));
+		System.out.println("mm.add(3L,3L) 결과 :"+mm.add(3L,3L));
+		
+		int[]a = {100,200,300};
+		System.out.println("mm.add(a) 결과:"+mm.add(a));
+	}
+}
+class MyMath3{
+	int add(long l, long m) {
+		System.out.print("int add(int a, int b)-");
+		return (int) (l+m);
+	}
+	
+	int add(int[]a) {
+		System.out.print("int add(int[] a)-");
+		int result = 0;
+		for(int i = 0; i<a.length; i++) {
+			result +=a[i];
+		}
+		return result;
+	}
+}
